6fe5020500ed07b761ee9564c1b2802673f9d700
DamarisMenfer <damaris.menfer@gmail.com>
2018-03-15 09:51:50 +0100

    Pop-up again

---
+++ Test/Assets/Scripts/ButtonMainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ButtonMainMenu : MonoBehaviour {
+
+	private bool panelOpened = false;
+
+	private GameObject SubPanel = null;
+
+	void Awake(){
+		SubPanel = GameObject.Find("Canvas").transform.Find( "SubPanel" ).gameObject;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void TogglePanelOpen(){
+		panelOpened = !panelOpened;
+		SetSubPanelVisibility();
+	}
+
+	private void SetSubPanelVisibility(){
+		if (SubPanel != null) {
+			SubPanel.SetActive(panelOpened);
+		}
+	}
+}
