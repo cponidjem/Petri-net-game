d285b0fd09d35b6e9fa214b4ccaeffabe7eeba39
Tatu Makinen <tatu.makinen@student.tut.fi>
2018-03-15 08:44:50 +0100

    Scene transitions and cleaning up project

---
+++ Test/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Prime31.TransitionKit;
+
+public class SceneTransition : MonoBehaviour {
+
+    public int nextSceneID;
+
+    public void ChangeScene()
+    {
+        FadeTransition fader = new FadeTransition()
+        {
+            nextScene = nextSceneID,
+            fadedDelay = 0.2f,
+            fadeToColor = Color.black
+        };
+        TransitionKit.instance.transitionWithDelegate(fader);
+    }
+}
