f8ce20b9ebda38ac7027064f2ea1a351a2afa52e
cponidjem <cindy.ponidjem@outlook.fr>
2018-04-29 15:11:25 +0200

    added addArc (perform and canPerform) and targetPetriNetReached methods to the GameModel

--- Test/Assets/Scripts/MVC/GameModel.cs
+++ Test/Assets/Scripts/MVC/GameModel.cs
@@ -88,11 +88,20 @@ public class GameModel : GameElement
     private List<Place> places;
     private List<Transition> transitions;
 
-    public void initialisation(List<Place> places, List<Transition> transitions)
+	private List<Place> targetPlaces;
+
+	/*public void initialisation(List<Place> places, List<Transition> transitions, List<Place> targetPlaces)
     {
         this.places = places;
         this.transitions = transitions;
-    }
+		this.targetPlaces = targetPlaces;
+    }*/
+
+	public void initialisation(List<Place> places, List<Transition> transitions)
+	{
+		this.places = places;
+		this.transitions = transitions;
+	}
 
     public List<Place> performFire(int id)
     {
@@ -119,9 +128,64 @@ public class GameModel : GameElement
         return false;
     }
 
+	public bool canPerformAddArc(int idTransition, int idPlace, int coeff){
+		bool transitionExists = false;
+		bool placeExists = false;
+		foreach (Transition transition in transitions) {
+			if (transition.id == idTransition) {
+				transitionExists = true;
+			}
+		}
+		foreach(Place place in places){
+			if (place.id == idPlace) {
+				placeExists = true;
+			}
+		}
+		return transitionExists && placeExists;
+	}
+
+	public void performAddArc(int idTransition, int idPlace, int coeff, bool precondition){
+		foreach (Transition transition in transitions) {
+			if (transition.id == idTransition) {
+				if(precondition) {
+					transition.preconditions.Add (new Arc (idPlace, coeff));
+				} else {
+					transition.postconditions.Add (new Arc (idPlace, coeff));
+				}
+			}
+		}
+	}
+
     public List<Place> getPlaces()
     {
         return this.places;
-}
-}
+	}
 
+	public bool targetPetriNetReached(){
+		for (int i = 0; i < places.Count; i++) {
+			if(!places[i].id.Equals(targetPlaces[i].id) || !places[i].marking.Equals(targetPlaces[i].marking)){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/*void Start(){
+		places = new List<Place> ();
+		transitions = new List<Transition> ();
+		places.Add (new Place(1,1));
+		places.Add (new Place(2,0));
+		transitions.Add (new Transition (1, new List<Arc>(), new List<Arc>()));
+		performAddArc (1, 1, 1, true);
+		performAddArc (1, 2, 1, false);
+		targetPlaces = new List<Place> ();
+		targetPlaces.Add (new Place(1,0));
+		targetPlaces.Add (new Place(2,1));
+		Debug.Log (targetPetriNetReached());
+
+		if (canPerformFire (1)) {
+			performFire (1);
+		}
+		Debug.Log (targetPetriNetReached());
+	}*/
+}
