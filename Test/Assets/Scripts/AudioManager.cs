b7d19c356751290018d61ab6b3761f389e787aab
Vanessa Pinales <33542130+vpc97@users.noreply.github.com>
2018-05-15 02:04:31 +0200

    Working Main Menu

---
+++ Test/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour {
+    
+    public static AudioManager instance;
+
+    //public Sound[] sounds;
+    public Slider mySlider;
+    public AudioSource myMusic;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
+       /* foreach(Sound s in sounds){
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }*/
+
+    }
+
+    public void ToggledSound()
+    {
+        if (PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            PlayerPrefs.SetInt("Muted", 1);
+            //AudioListener.volume = 1;
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Muted", 0);
+            //AudioListener.volume = 0;
+
+        }
+
+    }
+    /*
+    void Start() {
+        Play("Theme");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.Log("Sound:" + name + " not found!");
+            return;
+        }
+            
+        s.source.Play();
+    }*/
+
+    public void SetVolume(float volume)
+    {
+
+        myMusic.volume = mySlider.value;
+    }
+
+    
+}
---
+++ Test/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour {
+
+    private AudioManager music;
+    public Button musicButton;
+    public Sprite musicOnSprite;
+    public Sprite musicOffSprite;
+
+    void Start()
+    {
+        music = GameObject.FindObjectOfType<AudioManager>();
+        UpdateIconAndVolume();
+    }
+
+    public void PauseMusic()
+    {
+        music.ToggledSound(); //update our player prefs
+        UpdateIconAndVolume();
+    }
+
+    public void UpdateIconAndVolume()
+    {
+        if (PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            AudioListener.volume = 1;
+            musicButton.GetComponent<Image>().sprite = musicOnSprite;
+        }
+        else
+        {
+            AudioListener.volume = 0;
+            musicButton.GetComponent<Image>().sprite = musicOffSprite;
+        }
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+
+}
