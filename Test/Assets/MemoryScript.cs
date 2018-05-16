d50e5429bc0f9188006516406b35b6f76ab0b300
Vanessa Pinales <33542130+vpc97@users.noreply.github.com>
2018-05-16 11:29:14 +0200

    add memory & back instructions button

--- Test/Assets/MemoryScript.cs
+++ Test/Assets/MemoryScript.cs
@@ -1,21 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 // Thing where we can store information between scenes. Shoul be replaced when better solution emerges.
-public class MemoryScript : MonoBehaviour {
-
+public class MemoryScript : MonoBehaviour
+{
     private List<Place> targetPlaces;
     private int lastLevelCompleted;
 
-	public List<Place> getEndPlaces(){
-		return targetPlaces;
-	}
+    public List<Place> getEndPlaces()
+    {
+        return targetPlaces;
+    }
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         DontDestroyOnLoad(this.gameObject);
-	}
+        Load();
+    }
 
     public void setEndPlaces(List<Place> places)
     {
@@ -28,7 +34,38 @@ public class MemoryScript : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
-		
-	}
+    void Update()
+    {
+
+    }
+
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerProgress.dat");
+
+        PlayerProgress progress = new PlayerProgress();
+        progress.lastLevelCompleted = lastLevelCompleted;
+
+        bf.Serialize(file, progress);
+        file.Close();
+    }
+
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/playerProgress.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerProgress.dat", FileMode.Open);
+            PlayerProgress progress = (PlayerProgress)bf.Deserialize(file);
+            file.Close();
+            lastLevelCompleted = progress.lastLevelCompleted;
+        }
+    }
+}
+
+[Serializable]
+class PlayerProgress
+{
+    public int lastLevelCompleted;
 }
