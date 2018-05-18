3dbc7ae4df50cfa85a494d57c41f44f2057885fd
Tatu Makinen <tatu.makinen@student.tut.fi>
2018-05-18 19:46:35 +0200

    Change transition - might work

--- Test/Assets/Scripts/SceneTransition.cs
+++ Test/Assets/Scripts/SceneTransition.cs
@@ -16,6 +16,20 @@ public class SceneTransition : MonoBehaviour {
             fadedDelay = 0.2f,
             fadeToColor = Color.black
         };
-        TransitionKit.instance.transitionWithDelegate(fader);
+
+        var pixelater = new PixelateTransition()
+        {
+            nextScene = nextSceneID,
+            finalScaleEffect = PixelateTransition.PixelateFinalScaleEffect.ToPoint,
+            duration = 1.0f
+        };
+        if (Random.Range(0, 1) < 0.5F)
+        {
+            TransitionKit.instance.transitionWithDelegate(pixelater);
+        } else
+        {
+
+            TransitionKit.instance.transitionWithDelegate(fader);
+        }
     }
 }
